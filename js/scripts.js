2f4dc0ffe2f2064b3ec64387f97db9a0b2296085
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-26 10:55:26 -0700

    fix rollDice function logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,69 @@
-// back end
+// back end logic
 let player1, player2, rollResult;
-let btnRoll = $('button.btn-roll');
-let btnHold = $('button.btn-hold');
+let turnScore = 0;
+let gameScore = 0;
 
-function Player(name, roundScore, gameScore) {
+function Player(name, turnScore, gameScore, active) {
   this.name = name;
-  this.turnScore = 0;
+  this.turnScore = turnScore;
   this.gameScore = gameScore;
-  this.active = true;
+  this.active = active;
 }
-//start game button event listener
-$("#game-start").click(function(event) {
-  event.preventDefault();
-  player1 = new Player($("#player1-name").val());
-  player2 = new Player($("#player2-name").val());
-  console.log(player1.name + ", " + player2.name);
-});
 
-function randomNumber() {
+//rolls dice
+function rollDice() {
   rollResult = Math.floor(Math.random() *  6 ) + 1;
   return rollResult;
 }
 
-function numberEqualsOne() {
-  gameScore = 0;
-  console.log(`Sorry, ${this.name}! You rolled a 1 so your turn has ended and your current score is ${this.gameScore}.`)
+//checks number and calculates turn/game score
+function checkNumber(rollResult) {
+  if (rollResult === 1) {
+    turnScore = 0;
+    console.log(`Sorry, ${this.name}! You rolled a 1 so your turn score is ${turnScore}.`)
+  } else {
+    turnScore += rollResult;
+    console.log(`${this.name}, you rolled a ${rollResult}. Total score is currently ${turnScore}`);
+  }
 }
+
+//calculates current player game score
+function holdTurn() {
+
+}
+
+//switches to the other player
+function switchActivePlayer() {
+  
+}
+
+//checks if player game score is 100 and displays winner
+function winCondition(){
+
+}
+
+//front end logic
+
+//start button event listener
+$("button#game-start").click(function(event) {
+  event.preventDefault();
+  player1 = new Player($("#player1-name").val());
+  player2 = new Player($("#player2-name").val());
+  $("#player1-area").show();
+  $("#player2-area").show();
+})
 
 //roll button event listener
 $("button.btn-roll").click(function(event) {
   event.preventDefault();
-  randomNumber();
-  //checks if number equals 1
-  if (rollResult === 1) {
-    this.turnScore = 0;
-    numberEqualsOne();
-  } else {
-    console.log(`${this.name}, you rolled a ${rollResult}`);
-  }
-});
+  rollDice();
+  checkNumber(rollResult);
+})
 
 //hold button event listener
 $("button.btn-hold").click(function(event) {
   event.preventDefault();
+  holdTurn();
+  switchActivePlayer();
 })
 
-// function player1Enable() {
-//   $("#player1-area").toggle();
-//   $("#player2-area").hide();
-// }
-
-// function player2Enable() {
-//   $("#player2-area").toggle();
-//   $("#player1-area").hide();
-// }
