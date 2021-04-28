bd8c65fb1abd37c47717dd475495f008ecb3b16e
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-28 10:17:49 -0700

    finalize player class constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // back end logic
-let rollResult;
+let rollResult, player1, player2;
 
 class Player {
   constructor(name) {
@@ -8,87 +8,55 @@ class Player {
     this.gameScore = 0;
     this.rollResult = 0;
   }
+  rollDice() {
+    this.rollResult = Math.floor(Math.random() * 6) + 1;
+    this.checkNumber(this.rollResult);
+    console.log(this.rollResult);
+  }
+  checkNumber() {
+    if (this.rollResult === 1) {
+      this.turnScore = 0;
+      this.endTurn();
+    } else if (this.rollResult !== 1) {
+      this.turnScore += this.rollResult;
+    }
+  }
+  endTurn() {
+    this.gameScore += this.turnScore;
+    this.turnScore = 0;
+    this.checkForWin();
+    if(this.name === player1.name) {
+      player1Switch();
+    } else {
+      player2Switch();
+    }
+  }
+  checkForWin() {
+    if (this.gameScore >= 100) {
+      this.endGame();
+    } 
+  }
 }
 
-let player1 = new Player();
-let player2 = new Player();
+//front end logic
 
-// let player1 = {
-//   turnScore: 0,
-//   gameScore: 0,
-//   rollResult: 0,
-//   rollDice() {
-//     rollResult = Math.floor(Math.random() * 6) + 1;
-//     this.checkNumber(rollResult);
-//   },
-//   checkNumber() {
-//     if (rollResult === 1) {
-//       this.turnScore = 0;
-//       this.endTurn();
-//     } else if (rollResult !== 1) {
-//       this.turnScore += rollResult;
-//     }
-//   },
-//   endTurn() {
-//     this.gameScore += this.turnScore;
-//     this.turnScore = 0;
-//     this.checkForWin();
-//   },
-//   switchPlayer() {
-//     $('#player2-area').removeClass('disableGamingArea');
-//     $('#player1-area').addClass('disableGamingArea');
-//     $('button#player1-roll').prop('disabled', true);
-//     $('button#player1-hold').prop('disabled', true);
-//     $('button#player2-roll').prop('disabled', false);
-//     $('button#player2-hold').prop('disabled', false);
-//   },
-//   checkForWin() {
-//     if (this.gameScore >= 100) {
-//       endGame();
-//     } else {
-//       this.switchPlayer();
-//     }
-//   },
-// }
+function player1Switch() {
+  $('#player2-area').removeClass('disableGamingArea');
+  $('#player1-area').addClass('disableGamingArea');
+  $('button#player1-roll').prop('disabled', true);
+  $('button#player1-hold').prop('disabled', true);
+  $('button#player2-roll').prop('disabled', false);
+  $('button#player2-hold').prop('disabled', false);
+}
 
-// let player2 = {
-//   turnScore: 0,
-//   gameScore: 0,
-//   rollResult: 0,
-//   rollDice() {
-//     rollResult = Math.floor(Math.random() * 6) + 1;
-//     this.checkNumber(rollResult);
-//   },
-//   checkNumber() {
-//     if (rollResult === 1) {
-//       this.turnScore = 0;
-//       this.endTurn();
-//     } else if (rollResult !== 1) {
-//       this.turnScore += rollResult;
-//     }
-//   },
-//   endTurn() {
-//     this.gameScore += this.turnScore;
-//     this.turnScore = 0;
-//     this.switchPlayer();
-//     this.checkForWin();
-//   },
-//   switchPlayer() {
-//     $('#player1-area').removeClass('disableGamingArea');
-//     $('#player2-area').addClass('disableGamingArea');
-//     $('button#player1-roll').prop('disabled', false);
-//     $('button#player1-hold').prop('disabled', false);
-//     $('button#player2-roll').prop('disabled', true);
-//     $('button#player2-hold').prop('disabled', true);
-//   },
-//   checkForWin() {
-//     if (this.gameScore >= 100) {
-//       endGame();
-//     } else {
-//       this.switchPlayer();
-//     }
-//   },
-// }
+function player2Switch() {
+  $('#player1-area').removeClass('disableGamingArea');
+  $('#player2-area').addClass('disableGamingArea');
+  $('button#player1-roll').prop('disabled', false);
+  $('button#player1-hold').prop('disabled', false);
+  $('button#player2-roll').prop('disabled', true);
+  $('button#player2-hold').prop('disabled', true);
+}
 
 function clearScores(){
   player1.turnScore = 0;
@@ -98,11 +66,20 @@ function clearScores(){
   $("#player1-area").hide();
   $("#player2-area").hide();
 }
-//front end logic
+
+//end game
+function endGame() {
+  clearScores();
+  alert("Winner!");
+  $("#new-game").show();
+  $("#dice-display").hide();
+}
 
 //start button event listener
 $("button#game-start").click(function(event) {
   event.preventDefault();
+  player1 = new Player($("#player1-name").val());
+  player2 = new Player($("#player2-name").val());
   $("#player-names").hide();
   $("#player1-name-display").append($("#player1-name").val());
   $("#player2-name-display").append($("#player2-name").val());
@@ -136,6 +113,7 @@ $("button#player2-roll").click(function(event) {
 $("button#player1-hold").click(function(event) {
   event.preventDefault();
   player1.endTurn();
+  player1Switch();
   $("#player1-gamescore").text(player1.gameScore);
   $("#player1-turnscore").text("");
   $("#player1-rollscore").text("");
@@ -145,21 +123,16 @@ $("button#player1-hold").click(function(event) {
 $("button#player2-hold").click(function(event) {
   event.preventDefault();
   player2.endTurn();
+  player2Switch();
   $("#player2-gamescore").text(player2.gameScore);
   $("#player2-turnscore").text("");
   $("#player2-rollscore").text("");
 })
 
+//new game button event listener
 $("button#new-game").click(function() {
   $("#player-names").show();
   $("input#player1-name").val("");
   $("input#player2-name").val("");
   $("#new-game").hide();
 })
-
-function endGame() {
-  clearScores();
-  alert("Winner!");
-  $("#new-game").show();
-  $("#dice-display").hide();
-}
